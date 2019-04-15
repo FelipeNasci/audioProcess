53f2378f7350e904f6fe25cb8c3974b2596cc588
felipeNasci <dfnascimento@live.com>
2019-04-15 08:32:50 -0300

    init work image

--- Use.py
+++ Use.py
@@ -1,18 +1,33 @@
 from DCT import DCT
+from Image import Image
 from Grafico import Grafico
 
 dct = DCT()
+img = Image()
 grafico = Grafico()
+
+#lena = um vetor [][]
+lena = img.getImage('lena.bmp')
+#img.showImage("Lena Original", lena)
+dct.dctImg(lena)
+
+
+
+
+
+'''
 
 sDigital = [10, 5, 8.5, 2, 1, 1.5, 0, 0.1]     #   Sinal Digital
 print('Sinal original')
 print(sDigital)
-grafico.plotPonto(sDigital, "Sinal Original")
+#grafico.plotPonto(sDigital, "Sinal Original")
 
 sAnalogico = dct.dct(sDigital)
 print(sAnalogico)
-grafico.plotPonto(sAnalogico, "Sinal Analogico (DCT)")
+#grafico.plotPonto(sAnalogico, "Sinal Analogico (DCT)")
 
 sDigital = dct.dct(sAnalogico)
 print(sDigital)
-grafico.plotPonto(sDigital, "Sinal Digital (IDCT)")
+#grafico.plotPonto(sDigital, "Sinal Digital (IDCT)")
+
+'''
