bf083ffda13cf234259c08d8d706c225af9f5efe
FelipeNasci <dfnascimento@live.com>
2019-04-18 17:43:40 -0300

    dct and idct

--- DCT.py
+++ DCT.py
@@ -3,26 +3,43 @@ from math import pow
 from math import pi
 class DCT:
 
-    x = None
-
     def dct(self, X):
         N = len(X)                              #   Numero de amostras
-        self.x = [ [0] * N ] * N
+        x = [ [0] * N ] * N
         sinal = [0] * N
 
-        for k in range (0, N):
+        for k in range(0, N):
 
-            ck = pow(1/2, 1/2)
-            if k > 0: ck = 1
+            ck = 1
+            if k == 0: ck = pow(1/2, 1/2)
 
-            Ak = pow(2/N, 1/2) * ck * X[k]
+            Ak = pow(2/N, 1/2)
             fk = k / (2 * N)
             phase = (k * pi) / (2 * N)
 
             for n in range(0, N):
-                self.x[k][n] = round(Ak * cos(2 * pi * fk * n + phase), 4 ) #Arredonda em 4 casas decimais
-                sinal[n] += self.x[k][n]
-            #print(self.x[k])
+                x[k][n] = X[n] * cos(2 * pi * fk * n + phase) #Arredonda em 4 casas decimais
+                sinal[k] += Ak * ck * x[k][n]
+        return sinal
+
+
+    def idct(self, X):
+        N = len(X)                              #   Numero de amostras
+        x = [ [0] * N ] * N
+        sinal = [0] * N
+
+        for k in range(0, N):
+
+            ck = 1
+            if k == 0: ck = pow(1/2, 1/2)
+
+            Ak = pow(2/N, 1/2) * ck
+            fk = k / (2 * N)
+            phase = (k * pi) / (2 * N)
+
+            for n in range(0, N):
+                x[k][n] = X[k] * cos(2 * pi * fk * n + phase) #Arredonda em 4 casas decimais
+                sinal[n] += Ak * x[k][n]
         return sinal
 
     #realiza a soma de todos os elementos do vetor
--- Use.py
+++ Use.py
@@ -4,30 +4,14 @@ from Grafico import Grafico
 
 dct = DCT()
 img = Image()
-grafico = Grafico()
-
-#lena = um vetor [][]
-lena = img.getImage('lena.bmp')
-#img.showImage("Lena Original", lena)
-dct.dctImg(lena)
-
-
-
-
-
-'''
+#grafico = Grafico()
 
 sDigital = [10, 5, 8.5, 2, 1, 1.5, 0, 0.1]     #   Sinal Digital
 print('Sinal original')
 print(sDigital)
-#grafico.plotPonto(sDigital, "Sinal Original")
 
-sAnalogico = dct.dct(sDigital)
+sAnalogico = dct.idct(sDigital)
 print(sAnalogico)
-#grafico.plotPonto(sAnalogico, "Sinal Analogico (DCT)")
 
 sDigital = dct.dct(sAnalogico)
 print(sDigital)
-#grafico.plotPonto(sDigital, "Sinal Digital (IDCT)")
-
-'''
